88813b34714d7df2b4c4ee149d448a5c59191a25
Taufik Rahadi <taufik.rahadi@akarinti.tech>
2023-05-17 08:59:12 +0700

    fix: isExists value is a number

--- lib/validator/is-unique.validator.ts
+++ lib/validator/is-unique.validator.ts
@@ -33,7 +33,7 @@ export class IsUniqueConstraints implements ValidatorConstraintInterface {
         [key]: value,
       });
 
-      return isExists.length < 0;
+      return isExists < 0;
     } catch (error) {
       return false;
     }
