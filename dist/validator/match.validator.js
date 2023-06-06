20b24ffcef4b096950edc060688201f35debbb77
Taufik Rahadi <taufik.rahadi@akarinti.tech>
2023-06-06 10:30:58 +0700

    build: matchvalidator

--- dist/validator/match.validator.js
+++ dist/validator/match.validator.js
@@ -1,4 +1,10 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.MatchConstraints = exports.Match = void 0;
 const class_validator_1 = require("class-validator");
@@ -14,11 +20,14 @@ function Match(propertyName, validationOptions) {
     };
 }
 exports.Match = Match;
-class MatchConstraints {
+let MatchConstraints = class MatchConstraints {
     validate(value, validationArguments) {
         const [relatedPropertyName] = validationArguments.constraints;
         const relatedValue = validationArguments.object[relatedPropertyName];
         return value === relatedValue;
     }
-}
+};
+MatchConstraints = __decorate([
+    (0, class_validator_1.ValidatorConstraint)({ name: 'Match', async: false })
+], MatchConstraints);
 exports.MatchConstraints = MatchConstraints;
